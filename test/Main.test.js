f1d9cea765fbf7e0dbf2b51a6153347c51c2d5cc
Uzair Shamim <uzashamim@gmail.com>
2018-08-12 18:53:49 -0400

    removed flush

--- test/Main.test.js
+++ test/Main.test.js
@@ -3,22 +3,9 @@ import { render } from "preact-render-spy";
 import { h } from "preact";
 
 describe("SearchBar component ", () => {
-  beforeAll(() => {
-    global.browser = browser;
-  });
-
-  beforeEach(() => {
-    browser.flush();
-  });
-
   it("should rendered", () => {
     const props = {};
 
     const mainComponentRender = render(<Main {...props} />);
-  });
-
-  afterAll(() => {
-    browser.flush();
-    delete global.browser;
   });
 });
