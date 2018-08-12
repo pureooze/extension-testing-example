8cda39a047d09475f71a5af5822fab7b03b05aa3
Uzair Shamim <uzashamim@gmail.com>
2018-08-12 19:06:17 -0400

    Added sinon-chrome

--- test/Main.test.js
+++ test/Main.test.js
@@ -1,10 +1,25 @@
+const chrome = require("sinon-chrome/extensions");
 import Main from "src/example/index.jsx";
 import { render } from "preact-render-spy";
 import { h } from "preact";
 
 describe("Main component ", () => {
+  beforeAll(() => {
+    global.chrome = chrome;
+  });
+
+  beforeEach(() => {
+    chrome.flush();
+  });
+
   it("should rendered", () => {
     const props = {};
+    chrome.runtime.getURL.returns("http://localhost/popup-content.html");
     const mainComponentRender = render(<Main {...props} />);
+  });
+
+  afterAll(() => {
+    chrome.flush();
+    delete global.chrome;
   });
 });
