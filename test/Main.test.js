9f97ce980facebf698fc8fe739de1e792b4f2cde
Uzair Shamim <uzashamim@gmail.com>
2018-08-12 18:51:58 -0400

    stash apply

--- test/Main.test.js
+++ test/Main.test.js
@@ -1,11 +1,25 @@
+const browser = require("sinon-chrome/webextensions");
 import Main from "src/example/index.jsx";
 import { render } from "preact-render-spy";
 import { h } from "preact";
 
 describe("SearchBar component ", () => {
+  beforeAll(() => {
+    global.browser = browser;
+  });
+
+  beforeEach(() => {
+    browser.flush();
+  });
+
   it("should be empty when no there is no search string provided", () => {
     const props = {};
 
     const searchBarRender = render(<Main {...props} />);
+  });
+
+  afterAll(() => {
+    browser.flush();
+    delete global.browser;
   });
 });
