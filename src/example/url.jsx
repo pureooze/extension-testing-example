3714ad1022766fdbc47020f4e48e07315bc9b737
Uzair Shamim <uzashamim@gmail.com>
2018-08-12 18:38:55 -0400

    Added preact components

---
+++ src/example/url.jsx
@@ -0,0 +1,24 @@
+import { Component, h } from "preact";
+
+export default class Url extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      site: ""
+    };
+  }
+
+  async componentWillMount() {
+    const site = await this.props.getTopSites();
+    this.setState({
+      site
+    });
+  }
+
+  render() {
+    chrome.extension.getBackgroundPage().console.log(this.state.site);
+
+    return <b>{this.state.site}</b>;
+  }
+}
